d91d6bbbb40c7f73355d9d0c41511f1be3657170
AbbyEnRedes <abbyenredes@gmail.com>
2025-04-25 13:53:52 +0200

    chore: remove old files

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from model.predictor import predict_price, MODELS
 
-app = FastAPI()
-
+app = FastAPI(title="Vehicle Price API")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World hola"}
+def read_root():
+    return {"status": "OK", "endpoints": ["/predict", "/docs"]}
+
+class Vehicle(BaseModel):
+    year: int
+    odometer: float
+    cylinders: str
+    manufacturer: str
+    paint_color: str
+    state: str
+
+@app.post("/predict")
+def get_prediction(
+    vehicle: Vehicle,
+    model: str = Query("rf", enum=list(MODELS.keys()))
+):
+    price = predict_price(model, vehicle.dict())
+    return {"predicted_price": price}
+
